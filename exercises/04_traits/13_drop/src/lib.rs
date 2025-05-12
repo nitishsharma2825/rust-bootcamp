f9a8d2a5f391e307f8f00b71a66a27244b793d4e
nitishsharma2825 <sharnit2825@gmail.com>
2025-05-12 21:25:29 +0530

    more traits

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,28 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    active: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        DropBomb { active: true }
+    }
+
+    pub fn defuse(&mut self) {
+        self.active = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.active {
+            panic!("Bomb exploded!");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
